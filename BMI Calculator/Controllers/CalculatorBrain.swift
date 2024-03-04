617490674360756136d04718947d6a3cfcf3a7af
Kim Bui <81892049+thanhkimbui@users.noreply.github.com>
2024-03-03 16:51:11 -0800

    Complete

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -7,17 +7,33 @@
 //
 
 import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmiValue: Float?
-    
-    mutating func calculateBMI(height: Float, weight: Float) -> Float {
-        self.bmiValue = weight / pow(height, 2)
-        return bmiValue
-    }
+    var bmi: BMI?
     
     func getBMIValue() -> String {
-        return String(format: "%.1f", self.bmiValue)
+        return String(format: "%.1f", bmi?.value ?? 0.0)
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight / (height * height)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.blue)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: UIColor.green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less sugar!", color: UIColor.red)
+        }
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.black
     }
 }
